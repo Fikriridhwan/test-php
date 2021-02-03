786f588e97943a218a18fb3f0e68d66fe2752d10
Fikriridhwan <fikriridhwan@gmail.com>
2021-02-03 22:18:53 +0700

    Update koneksi.php

--- bank/bank/koneksi.php
+++ bank/bank/koneksi.php
@@ -1,14 +1,12 @@
 <?php
 
-use PDO;
-
 $host = "192.168.0.8"; // Nama hostnya
 $username = "root"; // Username
 $password = "my-root"; // Password (Isi jika menggunakan password)
 $database = "bank_crud"; // Nama databasenya
 // Koneksi ke MySQL dengan PDO
 
-$pdo = mysqli_connect($host,$username,$password,$database);
+$pdo = new mysqli($host,$username,$password,$database);
 
 
 // $dns = "mysql:host=".getenv('http://ip172-18-0-12-c0daeqs34gag00ac6cgg-3306.direct.labs.play-with-docker.com/')."; dbname=".getenv('bank_crud');
